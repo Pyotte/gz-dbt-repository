aad26c6c952816ac2a7fca6358ea0eae55c8c566
benjamin.l.py@gmail.com <benjamin.l.py@gmail.com>
2024-05-07 14:12:00 +0000

    base propre

--- models/intermediate/int_orders_margin.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    orders_id
-    ,date_date
-    ,ROUND(SUM(revenue),2) AS revenue
-    ,ROUND(SUM(purchase_price),2) purchase_cost
-    ,ROUND(SUM(margin),2) AS margin
-FROM {{ ref('int_sales_margin') }}
-GROUP BY orders_id, date_date
-ORDER BY orders_id DESC
