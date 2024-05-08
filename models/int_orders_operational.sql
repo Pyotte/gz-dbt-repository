99e3870c4119d51f21ead138393e751d5b5e1600
benjamin.l.py@gmail.com <benjamin.l.py@gmail.com>
2024-05-08 08:12:50 +0000

    ajout purchase_cost

--- models/int_orders_operational.sql
+++ models/int_orders_operational.sql
@@ -3,6 +3,7 @@ SELECT
     ,margin.date_date
     ,margin.revenue
     ,margin.margin
+    ,margin.purchase_cost
     ,ROUND((margin.margin+shipping.shipping_fee-shipping.logcost-shipping.ship_cost),2) AS operational_margin
 FROM {{ ref('int_orders_margin') }} AS margin
 JOIN {{ ref('stg_raw__ship') }} AS shipping
